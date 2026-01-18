2bf5aeae7338631b9b681a81630dfb36ffef5ced
hesonthemove-ux <hesonthemove@gmail.com>
2026-01-18 13:04:34 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,31 @@
+// config.js
+// Central configuration for Rock.Scot site
 
+export const STATION_CONFIG = {
+
+  meta: {
+    email: "sales@rock.scot",
+    streamUrl: "https://player.broadcast.radio/caledonia-tx-ltd",
+    logo: "./assets/images/branding/logo_ultra_wide.png"
+  },
+
+  backgrounds: [
+    "./assets/images/background/background1.jpg",
+    "./assets/images/background/background2.jpg",
+    "./assets/images/background/background3.jpg",
+    "./assets/images/background/background4.jpg",
+    "./assets/images/background/background5.jpg",
+    "./assets/images/background/background6.jpg",
+    "./assets/images/background/background7.jpg"
+  ],
+
+  prices: {
+    packages: [
+      { id: 299, name: "Single Region (£299)" },
+      { id: 449, name: "Multi-Region (£449)" },
+      { id: 2500, name: "Top-of-Hour Exclusive (£2,500/mo)" }
+    ],
+    productionFee: 195,
+    surchargeThreshold: 28,   // days
+    surchargeRate: 1.3
+      
